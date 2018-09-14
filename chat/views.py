4657750d92cca33a3e1d21f04bc2085755812429
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2018-09-14 23:35:12 +1000

    fixed fe

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,3 @@
-# chat/views.py
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
@@ -6,7 +5,8 @@ import json
 def index(request):
     return render(request, 'chat/index.html', {})
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
-    })
+    # return render(request, 'chat/room.html', {
+    #     'room_name_json': mark_safe(json.dumps(room_name))
+    # })
+
+    
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('chat.urls')),
+    # main root points to chat app
+    url(r'^chat/$', include('chat.urls')),
     url(r'^admin/', admin.site.urls),
 ]
