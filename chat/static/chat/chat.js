15b345449fd7fed59091189a2116ba096d49c91d
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2018-09-15 01:41:15 +1000

    conversation is workgin

--- chat/static/chat/chat.js
+++ chat/static/chat/chat.js
@@ -1,20 +1,4 @@
-// function insertMessage() {
-//     console.log('insert msg..')
-//     msg = $('.message-input').val();
-//     if ($.trim(msg) == '') {
-//         return false;
-//     }
-//     $('<div class="message message-personal">' + msg + '</div>').appendTo(
-//         $('.mCSB_container')
-//     ).addClass('new');
 
-//     setDate();
-//     $('.message-input').val(null);
-//     updateScrollbar();
-//     setTimeout(function () {
-//         fakeMessage();
-//     }, 1000 + (Math.random() * 20) * 100);
-// }
 
 // $('.message-submit').click(function () {
 //     insertMessage();
