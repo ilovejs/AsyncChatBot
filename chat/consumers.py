31525f9e63072a03c8d24c8d818e73c37d2c29f0
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2018-09-15 12:27:36 +1000

    refactory

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 # chat/consumers.py
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+# from string import strip
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,7 +14,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         print(text_data)
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json['message'].replace('\n','').strip()
+
+        if message in ['Male','Female']:
+            message = 'When were you born?'
+        elif message == '03-26-1989':        # Let user input date in (dd-mm-yyyy) format
+            message = 'Are you a smoker?'
+        else: # Yes or No
+            message = 'Thank you. Press "Done" for results.'
 
         # Send message to room group
         await self.send(text_data=json.dumps({
@@ -25,6 +33,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event['message']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
